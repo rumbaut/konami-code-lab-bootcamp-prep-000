054d9f22b76ae660bbc6f7208b9fa13ddd145801
Fernando R González <rumbaut2288@gmail.com>
2017-11-16 04:17:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
             var keyCode = var keyCode = (typeof e.which != "undefined") ? e.which : e.button;
-            //console.log(keyCode);
             e.stopPropagation;
             code.forEach(function(element) {
                 if(element === keyCode){
