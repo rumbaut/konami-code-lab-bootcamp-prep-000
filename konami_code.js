190a9c426ea1e621a64534ff798f9c0359d10f9a
Fernando R González <rumbaut2288@gmail.com>
2017-11-16 04:03:12 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 document.body.addEventListener("keydown",init, false);
 function init(e) {
-            var keyCode = e.keyCode;
+            var keyCode = parseInt(e.detail || e.which);
             //console.log(keyCode);
             e.stopPropagation;
             code.forEach(function(element) {
