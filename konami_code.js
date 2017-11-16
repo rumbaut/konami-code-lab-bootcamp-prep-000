a7c4943863d6d52bfa15bb0176c077418fe5dc10
Fernando R González <rumbaut2288@gmail.com>
2017-11-16 03:56:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 document.body.addEventListener("keydown",init, false);
 function init(e) {
   // Write your JavaScript code inside the init() function
-    var keyCode = e.keyCode;
+    var keyCode = e.wich;
     e.stopPropagation;
     code.forEach(function(element) {
         if(element === keyCode){
