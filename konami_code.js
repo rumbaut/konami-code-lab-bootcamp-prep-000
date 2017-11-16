71c202529167f0b9705a2a2e0fcaf119fea0f76e
Fernando R González <rumbaut2288@gmail.com>
2017-11-16 04:23:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,13 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
-            var keyCode = var keyCode = (typeof e.which != "undefined") ? e.which : e.button;
-            e.stopPropagation;
-            code.forEach(function(element) {
-                if(element === keyCode){
-                     alert(keyCode);
-                 }
-            }, this);
+  document.body.addEventListener("keydown",init, false);
+  var keyCode = var keyCode = (typeof e.which != "undefined") ? e.which : e.button;
+  e.stopPropagation;
+  code.forEach(function(element) {
+        if(element === keyCode){
+             alert(keyCode);
+       }
+    }, this);
+}
 
-          }
-document.body.addEventListener("keydown",init, false);
